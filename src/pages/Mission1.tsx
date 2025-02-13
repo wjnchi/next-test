90c60ddbfdc9691a16e48209ab28b3d85edc19ee
wangjianan <wangjianan.chi@bytedance.com>
2025-02-13 16:37:43 +0800

    修复了ts的一些错误

--- src/pages/Mission1.tsx
+++ src/pages/Mission1.tsx
@@ -1,10 +1,13 @@
 const Mission1: React.FC = () => {
    async function getMessage() {
     const data=await fetch('/api/hello')
-    return data.json()  
+    data.json().then((res)=>{
+      console.log(res.name)
+      return res
+    })
   }
   const data=getMessage()
-  console.log(data)
+  console.log('****'+data)
   return <div>111</div>;
 };
 
